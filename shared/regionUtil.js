820c213d77dc6f195a04a67ec8692f910be59ce5
probi5tics <jakeyseo0105@gmail.com>
2026-08-03 19:10:38 +0900

    Delete shared directory

--- shared/regionUtil.js
+++
@@ -1,5 +0,0 @@
-// js/multiplayer.js의 REGION_SIZE·regionKey와 반드시 동일한 값을 유지해야 함
-export const REGION_SIZE = 100;
-export function regionKeyOf(x, y) {
-  return `${Math.floor(x / REGION_SIZE)}_${Math.floor(y / REGION_SIZE)}`;
-}
